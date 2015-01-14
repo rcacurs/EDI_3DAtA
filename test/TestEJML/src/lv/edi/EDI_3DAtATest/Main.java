395f98370d0e92202a64fa1ccaa55a77110074da
Ričards Cacurs <rcacurs@gmail.com>
2015-01-14 17:17:16 +0200

    Create dense matrix, set values, output.

--- test/TestEJML/src/lv/edi/EDI_3DAtATest/Main.java
+++ test/TestEJML/src/lv/edi/EDI_3DAtATest/Main.java
@@ -1,9 +1,16 @@
 package lv.edi.EDI_3DAtATest;
 
+import org.ejml.data.DenseMatrix64F;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		DenseMatrix64F matrix = new DenseMatrix64F(3, 3);
+		matrix.add(1, 1, 1);
+		System.out.println("Matrix: "+matrix);
+		
 
 	}
 
