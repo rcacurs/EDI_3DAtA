446f8cdffbe43f45895cc9ab0eb5760e3b3193d9
Ričards Cacurs <rcacurs@gmail.com>
2015-01-26 16:57:19 +0200

    Function for convertin DenseMat64F as csv string.

---
+++ modules/src/lv/edi/EDI_3DAtA/common/Utils.java
@@ -0,0 +1,24 @@
+package lv.edi.EDI_3DAtA.common;
+import org.ejml.data.DenseMatrix64F;
+/**
+ * 
+ * @author Ričards Cacurs
+ * 
+ * This class is created for various utility functions for data formatting etc
+ * 
+ */
+public class Utils {
+	
+	public static String denseMatrix64FtoCSVString(DenseMatrix64F inputMatrix){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<inputMatrix.numRows; i++){
+			for(int j=0; j<inputMatrix.numCols-1; j++){
+				sb.append(inputMatrix.get(i,j)+",");
+			}
+			sb.append(inputMatrix.get(i, inputMatrix.numCols-1)+"\n");
+		}
+		return sb.toString();
+	}
+}
