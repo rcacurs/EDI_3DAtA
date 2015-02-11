b8c18e4b1677a149559482b4552106000cd0294d
Ričards Cacurs <rcacurs@gmail.com>
2015-02-11 10:20:48 +0200

    Test modification

--- test/FeatureExtractorTest/src/lv/edi/EDI_3DAtAtest/featureextractortest/Main.java
+++ test/FeatureExtractorTest/src/lv/edi/EDI_3DAtAtest/featureextractortest/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.swing.JFrame;
 
+import lv.edi.EDI_3DAtA.bloodvesselsegm.FilteringOperations;
 import lv.edi.EDI_3DAtA.bloodvesselsegm.LayerSMFeatures;
 import lv.edi.EDI_3DAtA.bloodvesselsegm.SMFeatureExtractor;
 import lv.edi.EDI_3DAtA.imageio.MetaImage;
@@ -36,44 +37,46 @@ public class Main {
 			return;
 		}
 		// READ LAYER DATA FROM IMAGE
-		try {
-			
-			selectedLayerIndex = Integer.parseInt(args[1]);
-		} catch (NumberFormatException e) {
-			System.out.println("Problem parcing specified layer. Specified layer must be specified as number!");
-			return;
-		}
-//		System.out.println("Reading layer...");
-		time1 = System.currentTimeMillis();
-		layerImage = metaImage.getLayerImage(selectedLayerIndex);
-		time2 = System.currentTimeMillis();
-//		System.out.println("Layer read time: "+(time2-time1)+" [ms]");
-		// SHOW LAYER
-//		ImageVisualization.imshow(layerImage, inputImageFrame);
-		// EXTRACT FEATURES
-//		System.out.println("Extracting Features... ");
-		featureExtractor = new SMFeatureExtractor("../../modules/res/dCodes", "../../modules/res/dMean", 5, 6);
-		time1 = System.currentTimeMillis();
-		while(true){
+		int z=0;
+		while(z<5){
+			z++;
+			try {
+				
+				selectedLayerIndex = Integer.parseInt(args[1]);
+			} catch (NumberFormatException e) {
+				System.out.println("Problem parcing specified layer. Specified layer must be specified as number!");
+				return;
+			}
 
+//			System.out.println("Reading layer...");
+			time1 = System.currentTimeMillis();
+			layerImage = metaImage.getLayerImage(selectedLayerIndex);
+			time2 = System.currentTimeMillis();
+//			System.out.println("Layer read time: "+(time2-time1)+" [ms]");
+			// SHOW LAYER
+//			ImageVisualization.imshow(layerImage, inputImageFrame);
+			// EXTRACT FEATURES
+//			System.out.println("Extracting Features... ");
+			featureExtractor = new SMFeatureExtractor("../../modules/res/dCodes", "../../modules/res/dMean", 5, 6);
+			time1 = System.currentTimeMillis();
 			layerFeatures = featureExtractor.extractLayerFeatures(layerImage);
 			time2 = System.currentTimeMillis();
-//			System.out.println("Feature extraction time: "+(time2-time1)+" [ms]");
+			System.out.println("Feature extraction time: "+(time2-time1)+" [ms]");
 			
-//			// VISUALIZING FEATURES
-//			JFrame[] featureFrames = new JFrame[args.length-2];
-//			for(int i=0; i<args.length-2; i++){
-//				int featureNum;
-//				try {
-//					featureNum=Integer.parseInt(args[2+i]);
-//				} catch (NumberFormatException e) {
-//					System.out.println("Error: Feature indexes must be specified as numbers!");
-//					return;
-//				}
-//				DenseMatrix64F feature0 = layerFeatures.getFeature(featureNum);
-//				featureFrames[i] = new JFrame("Feature "+featureNum+" frame");
-//				ImageVisualization.imshow(feature0, featureFrames[i]);
-//			}
+			// VISUALIZING FEATURES
+			JFrame[] featureFrames = new JFrame[args.length-2];
+			for(int i=0; i<args.length-2; i++){
+				int featureNum;
+				try {
+					featureNum=Integer.parseInt(args[2+i]);
+				} catch (NumberFormatException e) {
+					System.out.println("Error: Feature indexes must be specified as numbers!");
+					return;
+				}
+				DenseMatrix64F feature0 = layerFeatures.getFeature(featureNum);
+				featureFrames[i] = new JFrame("Feature "+featureNum+" frame");
+				ImageVisualization.imshow(feature0, featureFrames[i]);
+			}
 		}
 		
 	}
