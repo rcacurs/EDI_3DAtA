c71c95a8a27cd8bd6146d8eb7e608842b87d7f8e
Ričards Cacurs <rcacurs@gmail.com>
2015-02-17 14:04:40 +0200

    Creating gui for application.

---
+++ apps/Vessel-OBJ-Creator/src/lv/edi/EDI_3DAtA/vessel2objapp/AppController.java
@@ -0,0 +1,67 @@
+package lv.edi.EDI_3DAtA.vessel2objapp;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import lv.edi.EDI_3DAtA.imageio.MetaImage;
+import lv.edi.EDI_3DAtA.visualization.ImageVisualization;
+
+public class AppController implements Initializable{
+	private Stage mainStage;
+	
+	@FXML
+	private MenuItem menuItemOpenCTFile;
+	@FXML
+    private ImageView ctScanImageView;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+	public void setMainStage(Stage stage){
+		this.mainStage = stage;
+	};
+	
+	
+	
+	@FXML
+	public void selectCTScanFile(ActionEvent event){
+		System.out.println("Clicked");
+		System.out.println(menuItemOpenCTFile);
+		FileChooser fileChooser = new FileChooser();
+		ExtensionFilter extFilter = new ExtensionFilter("Meta Image files (*.mhd)", "*.mhd");
+		fileChooser.getExtensionFilters().add(extFilter);
+		fileChooser.setTitle("Open CT Scan File");
+		File scanFile = fileChooser.showOpenDialog(mainStage);
+		if(scanFile==null){
+			((MenuItem)event.getSource()).getParentMenu().hide();
+			return;
+		}
+		try {
+			Main.selectedTomographyScan= new MetaImage(scanFile);
+			System.out.println(Main.selectedTomographyScan.toString());
+			Main.currentLayerImage=Main.selectedTomographyScan.getLayerImage(Main.selectedLayer);
+			BufferedImage bImage = ImageVisualization.convDenseMatrixToBufImage(Main.currentLayerImage);
+			WritableImage image = new WritableImage(bImage.getWidth(), bImage.getHeight());
+			SwingFXUtils.toFXImage(bImage, image);
+			ctScanImageView.setImage(image);
+		} catch (IOException e) {
+			System.out.println("Problem Loadin Specified File");
+		}
+		
+		((MenuItem)event.getSource()).getParentMenu().hide();
+	}
+}
---
+++ apps/Vessel-OBJ-Creator/src/lv/edi/EDI_3DAtA/vessel2objapp/Main.java
@@ -0,0 +1,38 @@
+package lv.edi.EDI_3DAtA.vessel2objapp;
+	
+import org.ejml.data.DenseMatrix64F;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import lv.edi.EDI_3DAtA.imageio.MetaImage;
+
+
+public class Main extends Application {
+	static MetaImage selectedTomographyScan;
+	static int selectedLayer=100;
+	static DenseMatrix64F currentLayerImage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+			Parent root = loader.load();
+			AppController controller = loader.getController();
+			controller.setMainStage(primaryStage);
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			 
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
