763b5b14911c406490d92867155aab54f151c072
Ričards Cacurs <rcacurs@gmail.com>
2015-03-03 10:47:38 +0200

    Changing format of generated isosurface.

--- modules/src/lv/edi/EDI_3DAtA/marchingcubes/MarchingCubes.java
+++ modules/src/lv/edi/EDI_3DAtA/marchingcubes/MarchingCubes.java
@@ -4,6 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.concurrent.Task;
+
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
 
@@ -16,31 +20,71 @@ import lv.edi.EDI_3DAtA.common.VolumetricData;
  */
 public class MarchingCubes {
 	private VolumetricData data; // reference to targe data on wich marching cubes is to be performed
-	
+	private DoubleProperty progress; // progress for isosurface extraction
 	/**
 	 * Constructor for MarhincCubes iso-surface generator
 	 * @param data reference to VolumetricData object from which to extract iso-surface
 	 */
 	public MarchingCubes(VolumetricData data){
+		this.progress=new SimpleDoubleProperty();
 		this.data=data;
 	}
 	
-	/** Returns list of vertices for isosurface
-	 * 
-	 * @param isovalue - isovalue for wich surface is generated
-	 * @return ArrayList<DenseMatrix64F> of vertices for iso-surface
+//	/** Returns list of vertices for isosurface
+//	 * 
+//	 * @param isovalue - isovalue for wich surface is generated
+//	 * @return ArrayList<DenseMatrix64F> of vertices for iso-surface
+//	 */
+//	public ArrayList<DenseMatrix64F> generateIsoSurface(double isovalue){
+//		ArrayList<DenseMatrix64F> vertices = new ArrayList<DenseMatrix64F>(100);
+//		ArrayList<DenseMatrix64F> currentVertexList;
+//		DenseMatrix64F offset = new DenseMatrix64F(3,1);
+//		CubeMC cube = new CubeMC();
+//		if(data.size()!=0){
+//			int maxX = data.getLayer(0).numCols;
+//			int maxY = data.getLayer(0).numRows;
+//			int maxZ = data.size();
+//
+//			for(int k=0; k<maxZ-1; k++){
+//				
+//				offset.set(2, 0, k);
+//				for(int j=0; j<maxY-1; j++){
+//					offset.set(1,0, j);
+//					for(int i=0; i<maxX-1; i++){
+//						offset.set(0,0,i);
+//						cube.setVertexDensities(data.getValuesAtCubeVertices(i, j, k));
+//						currentVertexList=cube.getVertexList(isovalue);
+//						for(int ind=0; ind<currentVertexList.size(); ind++){
+//							CommonOps.add(currentVertexList.get(ind), offset, currentVertexList.get(ind));
+//						}
+//						vertices.addAll(currentVertexList);
+//						
+//					}
+//				}
+//			}
+//		}
+//		
+//		return vertices;
+//	}
+	/**
+	 * Method generates iso-surface model. Data is returned as TriangleMeshData.
+	 *  @param  isovalue - value at which to generate iso-surface
+	 *	@return TriangleMeshData - generated iso-surface in TriangleMeshData format.
 	 */
-	public ArrayList<DenseMatrix64F> generateIsoSurface(double isovalue){
-		ArrayList<DenseMatrix64F> vertices = new ArrayList<DenseMatrix64F>(100);
+	public TriangleMeshData generateIsoSurface(double isovalue){
+		ArrayList<Integer> facesal = new ArrayList<Integer>();
+		ArrayList<Float> vertices = new ArrayList<Float>();
 		ArrayList<DenseMatrix64F> currentVertexList;
+		
 		DenseMatrix64F offset = new DenseMatrix64F(3,1);
 		CubeMC cube = new CubeMC();
 		if(data.size()!=0){
 			int maxX = data.getLayer(0).numCols;
 			int maxY = data.getLayer(0).numRows;
 			int maxZ = data.size();
-			
+			int indexCounter=0;
 			for(int k=0; k<maxZ-1; k++){
+				progress.set((double)k/(maxZ-1));
 				offset.set(2, 0, k);
 				for(int j=0; j<maxY-1; j++){
 					offset.set(1,0, j);
@@ -51,13 +95,31 @@ public class MarchingCubes {
 						for(int ind=0; ind<currentVertexList.size(); ind++){
 							CommonOps.add(currentVertexList.get(ind), offset, currentVertexList.get(ind));
 						}
-						vertices.addAll(currentVertexList);
+						for(int z=0; i<currentVertexList.size(); i++){
+							vertices.add((float) currentVertexList.get(z).get(0));
+							vertices.add((float) currentVertexList.get(z).get(1));
+							vertices.add((float) currentVertexList.get(z).get(2));
+							facesal.add(indexCounter);
+							facesal.add(0); // add texture index as zero because tecture is not used
+							indexCounter++;
+						}
 						
 					}
 				}
 			}
 		}
-		return vertices;
+		progress.set(1);
+		float[] points = new float[vertices.size()];
+		for(int i=0; i<vertices.size(); i++){
+			points[i]=vertices.get(i);
+		}
+		int[] faces = new int[facesal.size()];
+		for(int i=0; i<facesal.size(); i++){
+			faces[i]=facesal.get(i);
+		}
+		float[] texCoords = new float[2];
+		return new TriangleMeshData(points, texCoords, faces);
+			
 	}
 	/** method that reduces number of vertexes by removing vertexes with same coordinates
 	 *  this method changes input ArrayList of points. The index list for triangulation 
@@ -112,5 +174,12 @@ public class MarchingCubes {
 			writer.println(indexS);
 		}
 		writer.close();
+	}
+	/**
+	 * Getter method for progress property
+	 * @return DoubleProperty progress of isovalue generation propery
+	 */
+	public DoubleProperty getProgressProperty(){
+		return progress;
 	}
 }
