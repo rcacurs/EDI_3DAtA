9760d937263719747d3ff8df0dfe0119306c4b52
Ričards Cacurs <rcacurs@gmail.com>
2015-03-25 10:32:35 +0200

    Setting up project properties to use opencv.

--- vs-cpp/Blood-Vessel-Segmentation-GPU/Blood-Vessel-Segmentation-GPU/main.cpp
+++ vs-cpp/Blood-Vessel-Segmentation-GPU/Blood-Vessel-Segmentation-GPU/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+
 using namespace std;
+using namespace cv;
+
 void main(){
+	Mat matrix(5, 5, CV_8UC1);
 	cout << "Works" << endl;
+	cout << matrix<<endl;
 	system("pause");
 }
