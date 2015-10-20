e583a6b10d3c8dae017c1414b1b90304dc2b1454
Ričards Cacurs <rcacurs@gmail.com>
2015-10-20 10:33:09 +0300

    Modifying test code

--- cuda-code/windowsTest/src/main.cpp
+++ cuda-code/windowsTest/src/main.cpp
@@ -2,19 +2,42 @@
 #include<imageMatrix.h>
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
-int main(){
-	double data[6]={1, 2, 3, 4, 5, 6};
-	
-	ImMatG *im = new ImMatG(3, 2, data, false);
-	double *resData = im->getData();
-	for(int i=0; i<im->getLength(); i++){
-		cout<<resData[i]<<endl;
+
+//convert to cv matrix
+
+int main(int argc, char *argv[]){
+	if(argc<3){
+		cout<<"Input image and lung mask should be specified!"<<endl;
+		return 1;
 	}
-	Mat test=Mat::eye(5, 5, CV_32FC1);
-	cout<<test<<endl;
-	delete im;
+	
+	// === READING TEST IMAGE USING OPENCV =============
+	Mat image = imread(argv[1]);
+	Mat imageMask = imread(argv[2]);
+	Mat imageGS;
+	Mat maskGS;
+	
+	cvtColor(imageMask, maskGS, CV_RGB2GRAY);
+	cvtColor(image, imageGS, CV_RGB2GRAY);
+	maskGS=maskGS/255;
+		
+	maskGS.convertTo(maskGS, CV_64FC1);
+	imageGS.convertTo(imageGS, CV_64FC1);
+	// === READING FILTER PARAMETERS =========
+	
+	ImMatG * dMeans = readCSV("./dMean.csv");
+	double *dt = dMeans->getData();
+	cout<<dMeans->getLength()<<endl;
+	for(int i=0; i<dMeans->getLength(); i++){
+		cout<<dt[i]<<" "<<endl;
+	}
+	
+	Mat output = (imageGS.mul(maskGS))/255;
+	imshow("Input image ", output);
+	waitKey(0);
 	return 0;
 }
