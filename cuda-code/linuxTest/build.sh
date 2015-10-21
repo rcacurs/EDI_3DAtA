9938e9cc762843e51792069fa015d79bdf905787
Ričards Cacurs <rcacurs@gmail.com>
2015-10-21 15:02:25 +0300

    Improving test code

--- cuda-code/linuxTest/build.sh
+++ cuda-code/linuxTest/build.sh
@@ -5,5 +5,5 @@ fi
 nvcc --include-path\
  "../bloodVesselSegmentation/include","$OPENCV_INCLUDE"\
  --library-path "../bloodVesselSegmentation/bin","$OPENCV_LIB"\
- --library opencv_core\
+ --library opencv_core,opencv_imgcodecs,opencv_highgui\
  --output-file ./bin/mainTest ./src/main.cpp
--- cuda-code/linuxTest/src/main.cpp
+++ cuda-code/linuxTest/src/main.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 #include<math.h>
 #include<opencv2/core/core.hpp>
+#include<opencv2/imgcodecs/imgcodecs.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
 using namespace std;
 using namespace cv;
-int main(){
+
+int main(int argc, char *argv[]){
+	if(argc<3){
+		cout <<"Input image and mask must be specified!"<<endl;	
+		return 1;
+	}
+	Mat image=imread(argv[1]);
+	Mat mask=imread(argv[2]);
+
 	Mat test(5, 5, CV_32FC1);
 	cout<<"works!"<<endl;
+	imshow("Test window",image);
+	imshow("Mask image", mask);
 	cout<<test<<endl;
+	waitKey(0);
 	return 0;
 }
