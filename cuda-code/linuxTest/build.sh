a2a0d2c0d27f2b1aa3023deb0c47242a5cd084a9
Ričards Cacurs <rcacurs@gmail.com>
2015-10-21 14:29:10 +0300

    Adding opencv library to test files

--- cuda-code/linuxTest/build.sh
+++ cuda-code/linuxTest/build.sh
@@ -1,1 +1,9 @@
-nvcc --output-file ./bin/mainTest ./src/main.cpp
+if [ ! -d "./bin" ]; then
+	mkdir bin
+fi
+
+nvcc --include-path\
+ "../bloodVesselSegmentation/include","$OPENCV_INCLUDE"\
+ --library-path "../bloodVesselSegmentation/bin","$OPENCV_LIB"\
+ --library opencv_core\
+ --output-file ./bin/mainTest ./src/main.cpp
--- cuda-code/linuxTest/src/main.cpp
+++ cuda-code/linuxTest/src/main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<math.h>
+#include<opencv2/core/core.hpp>
 using namespace std;
+using namespace cv;
 int main(){
-	
+	Mat test(5, 5, CV_32FC1);
 	cout<<"works!"<<endl;
+	cout<<test<<endl;
+	return 0;
 }
