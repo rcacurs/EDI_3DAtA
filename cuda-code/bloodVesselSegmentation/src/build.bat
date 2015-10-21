e8ba39303f98cedecda6908d2da2e7393fc16527
Ričards Cacurs <rcacurs@gmail.com>
2015-10-21 11:59:57 +0300

    Deleting junk

--- cuda-code/bloodVesselSegmentation/src/build.bat
+++
@@ -1,3 +0,0 @@
-call vcvars64.bat
-nvcc --device-c gpuBloodVesselSegmentation.cu gpuConvolution.cu gpuImageScaling.cu imageMatrix.cu
-nvcc --lib gpuBloodVesselSegmentation.obj gpuConvolution.obj gpuImageScaling.obj imageMatrix.obj --output-file segmentation.lib
