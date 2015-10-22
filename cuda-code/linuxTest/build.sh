45251074dfd406ed790d80c1e7f6343ac15e6067
Ričards Cacurs <rcacurs@gmail.com>
2015-10-22 10:22:35 +0300

    Working version of build cript for linux

--- cuda-code/linuxTest/build.sh
+++ cuda-code/linuxTest/build.sh
@@ -1,9 +1,11 @@
 if [ ! -d "./bin" ]; then
 	mkdir bin
 fi
+
 nvcc --include-path\
   ../bloodVesselSegmentation/include,$OPENCV_INCLUDE\
  --library-path ../bloodVesselSegmentation/bin,$OPENCV_LIB,$CUDA_PATH/lib64\
- --library cudadevrt,cudart,cublas,bvsegmentation,opencv_core,opencv_imgcodecs,opencv_highgui\
+ --library cublas_static,culibos,cudadevrt,cudart_static,\
+bvsegmentation,opencv_core,opencv_imgcodecs,opencv_highgui\
  --output-file ./bin/mainTest ./src/main.cpp
 
