5cbd335cafda3de630c8ef139a347c64dec430f1
Ričards Cacurs <rcacurs@gmail.com>
2015-10-26 15:09:22 +0200

    Removing cl command parameter

--- cuda-code/bloodVesselSegmentation/build.bat
+++ cuda-code/bloodVesselSegmentation/build.bat
@@ -1,10 +1,10 @@
 IF NOT EXIST .\bin mkdir bin
 
-nvcc -Xcompiler /wd4819 --device-c .\src\gpuBloodVesselSegmentation.cu --output-file .\bin\gpuBloodVesselSegmentation.obj
-nvcc -Xcompiler /wd4819 --device-c .\src\gpuConvolution.cu --output-file .\bin\gpuConvolution.obj
-nvcc -Xcompiler /wd4819 --device-c .\src\gpuImageScaling.cu --output-file .\bin\gpuImageScaling.obj
-nvcc -Xcompiler /wd4819 --device-c .\src\imageMatrix.cu --output-file .\bin\imageMatrix.obj
-nvcc -Xcompiler /wd4819 --lib --output-file .\bin\bvsegmentation.lib^
+nvcc --device-c .\src\gpuBloodVesselSegmentation.cu --output-file .\bin\gpuBloodVesselSegmentation.obj
+nvcc --device-c .\src\gpuConvolution.cu --output-file .\bin\gpuConvolution.obj
+nvcc --device-c .\src\gpuImageScaling.cu --output-file .\bin\gpuImageScaling.obj
+nvcc --device-c .\src\imageMatrix.cu --output-file .\bin\imageMatrix.obj
+nvcc --lib --output-file .\bin\bvsegmentation.lib^
  .\bin\imageMatrix.obj^
  .\bin\gpuConvolution.obj^
  .\bin\gpuImageScaling.obj^
