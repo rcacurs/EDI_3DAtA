9abd7e1e5785fc7ec7634d9815b967515c89c3e4
Ričards Cacurs <rcacurs@gmail.com>
2015-10-26 13:50:25 +0200

    Removing warnings from build script

--- cuda-code/bloodVesselSegmentation/build.bat
+++ cuda-code/bloodVesselSegmentation/build.bat
@@ -1,12 +1,13 @@
 IF NOT EXIST .\bin mkdir bin
 
-nvcc --device-c .\src\gpuBloodVesselSegmentation.cu --output-file .\bin\gpuBloodVesselSegmentation.obj
-nvcc --device-c .\src\gpuConvolution.cu --output-file .\bin\gpuConvolution.obj
-nvcc --device-c .\src\gpuImageScaling.cu --output-file .\bin\gpuImageScaling.obj
-nvcc --device-c .\src\imageMatrix.cu --output-file .\bin\imageMatrix.obj
-nvcc --lib --output-file .\bin\bvsegmentation.lib^
+nvcc -Xcompiler /wd4819 --device-c .\src\gpuBloodVesselSegmentation.cu --output-file .\bin\gpuBloodVesselSegmentation.obj
+nvcc -Xcompiler /wd4819 --device-c .\src\gpuConvolution.cu --output-file .\bin\gpuConvolution.obj
+nvcc -Xcompiler /wd4819 --device-c .\src\gpuImageScaling.cu --output-file .\bin\gpuImageScaling.obj
+nvcc -Xcompiler /wd4819 --device-c .\src\imageMatrix.cu --output-file .\bin\imageMatrix.obj
+nvcc -Xcompiler /wd4819 --lib --output-file .\bin\bvsegmentation.lib^
  .\bin\imageMatrix.obj^
  .\bin\gpuConvolution.obj^
  .\bin\gpuImageScaling.obj^
  .\bin\gpuBloodVesselSegmentation.obj
+ 
  
